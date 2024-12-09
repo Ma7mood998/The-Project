d10760e793aba652540bb4a4e7a46e1ff1baed8c
abrarAlqh <202011998@stu.uob.edu.bh>
2024-12-09 13:36:42 +0300

    Update user_room.php

--- user_room.php
+++ user_room.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once 'db.php'; // Include your PDO database connection file
+require_once 'db.php'; 
 
 if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
